3d3e7e7727bf7c088af703738550b606587bb49b
Admin <Admin@Admin-PC>
2026-03-03 20:48:06 +0700

    Init Project

---
+++ src/main/java/edu/poly/mainclass/polyproject/PolyProject.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package edu.poly.mainclass.polyproject;
+
+/**
+ *
+ * @author Admin
+ */
+public class PolyProject {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
